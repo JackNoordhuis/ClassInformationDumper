695261d0309e2d831e6017cf38c73cc6fc91bea0
Jack Noordhuis <daswaggamer@gmail.com>
2018-10-03 16:38:34 +1000

    namespace-info script now works

--- namespace-info.php
+++ namespace-info.php
@@ -18,9 +18,67 @@ declare(strict_types=1);
 
 require 'vendor/autoload.php';
 
+$ARGS = $argv;
+
+$FORMAT = 'json';
 $OUT = null;
 
-$pos = array_search('-o', $argv);
-if(!isset($pos) and $pos !== false) {
-    $OUT = $argv[$pos + 1];
+$NAMESPACE = null;
+
+array_shift($ARGS); // remove the script name from the arguments list
+
+$pos = array_search('-f', $ARGS);
+if(!isset($pos) or $pos !== false) {
+    $FORMAT = strtolower($ARGS[$pos + 1]);
+
+    switch ($FORMAT) {
+        case 'json':
+        case 'php':
+        case 'serialize':
+            fprintf(STDERR, 'Set output format to \'%s\'%s', $FORMAT, PHP_EOL);
+            break;
+        default:
+            fprintf(STDERR, 'Unsupported format specified \'%s\'%s', $ARGS[$pos + 1], PHP_EOL);
+            exit;
+    }
+
+    array_splice($ARGS, $pos,2); // remove argument from array
 }
+
+$pos = array_search('-o', $ARGS);
+if(!isset($pos) or $pos !== false) {
+    $OUT = __DIR__ . DIRECTORY_SEPARATOR . $ARGS[$pos + 1];
+
+    array_splice($ARGS, $pos,2); // remove argument from array
+}
+
+switch(count($ARGS)) {
+    case 1:
+        $NAMESPACE = $ARGS[0];
+        fprintf(STDERR,'Set scan namespace to \'%s\'%s',$NAMESPACE,PHP_EOL);
+        break;
+    default:
+        fprintf(STDERR,'Error:\\tToo much parameters are specified!%s',PHP_EOL);
+        exit;
+}
+
+$info = (new \jacknoordhuis\classinformationdumper\NamespaceInformation($NAMESPACE))->getClassInformation();
+
+if($OUT === null) {
+    fprintf(STDERR,'%s%s', json_encode($info), PHP_EOL);
+}
+$file = fopen($OUT, 'w') or die("Unable to open output file!\n");
+switch($FORMAT) {
+    case 'json':
+        fwrite($file, json_encode($info));
+        break;
+    case 'php':
+        fwrite($file, '<?php' . PHP_EOL . PHP_EOL);
+        fwrite($file, var_export($info, true) . ';');
+        break;
+    case 'serialize':
+        fwrite($file, serialize($info));
+        break;
+}
+
+fclose($file);
